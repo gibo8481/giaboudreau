06d8255223f978e02eb3f938a627d0365d4469ee
Gia <gibo8481@colorado.edu>
2025-06-24 15:16:25 -0600

    adding bloc images, updated project layout

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-// navbar.js
 function loadNavbar() {
   const navbar = document.getElementById("navbar");
 
   if (navbar) {
     navbar.innerHTML = `
       <nav class="vertical-nav">
-       <div class="profile">
+        <div class="profile">
           <img src="img/Gia Headshot 4.22 - Copy.jpg" alt="Gia Boudreau headshot" class="headshot" />
-          <p >Gianna Boudreau</p>
+          <p>Gianna Boudreau</p>
         </div>
         <ul>
           <li><a href="index.html">Home</a></li>
@@ -20,4 +19,37 @@ function loadNavbar() {
   }
 }
 
-document.addEventListener("DOMContentLoaded", loadNavbar);
+function setupCarousel() {
+    const carousels = document.querySelectorAll('.carousel-container');
+
+  carousels.forEach((carousel) => {
+    const images = Array.from(carousel.querySelectorAll('.carousel-img'));
+    const leftBtn = carousel.querySelector('.carousel-btn.left');
+    const rightBtn = carousel.querySelector('.carousel-btn.right');
+
+    let currentIndex = 0;
+
+    function showImage(index) {
+      images.forEach((img, i) => {
+        img.classList.toggle('active', i === index);
+      });
+    }
+
+    leftBtn.addEventListener('click', () => {
+      currentIndex = (currentIndex - 1 + images.length) % images.length;
+      showImage(currentIndex);
+    });
+
+    rightBtn.addEventListener('click', () => {
+      currentIndex = (currentIndex + 1) % images.length;
+      showImage(currentIndex);
+    });
+
+    showImage(currentIndex);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadNavbar();
+  setupCarousel();
+});
