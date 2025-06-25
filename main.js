9117aa6a19ac1905ba26aca9e9876cb68c0848c3
Gia <gibo8481@colorado.edu>
2025-06-25 16:46:15 -0600

    hamburger for mobile view

--- main.js
+++ main.js
@@ -8,16 +8,35 @@ function loadNavbar() {
           <img src="img/Gia Headshot 4.22 - Copy.jpg" alt="Gia Boudreau headshot" class="headshot" />
           <p>Gianna Boudreau</p>
         </div>
-        <ul>
-          <li><a href="index.html">Home</a></li>
-          <li><a href="about.html">About</a></li>
-          <li><a href="projects.html">Projects</a></li>
-          <li><a href="cv.html">CV</a></li>
+
+        <div class="hamburger" id="hamburger">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        <ul class="nav-links" id="navLinks">
+          <li><a href="index.html" class="nav-link">Home</a></li>
+          <li><a href="about.html" class="nav-link">About</a></li>
+          <li><a href="projects.html" class="nav-link">Projects</a></li>
+          <li><a href="cv.html" class="nav-link">CV</a></li>
         </ul>
       </nav>
     `;
+
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.getElementById('navLinks');
+
+    if (hamburger && navLinks) {
+      hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        navLinks.classList.toggle('active');
+      });
+    }
   }
 }
+
+
 
 function setupCarousel() {
     const carousels = document.querySelectorAll('.carousel-container');
